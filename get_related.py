697bf1d110e87e7536476565c408aac55522d385
AllenThomasDev <allen.thomas.dev@gmail.com>
2021-04-05 16:31:49 +0530

    comments removed from get_related.py

--- get_related.py
+++ get_related.py
@@ -29,19 +29,3 @@ def get_related(artist_id,depth,width=0):
         nodes.append({'data': {'id': x['uri'][15:], 'label': x['name']}})
         edges.append({'data': {'source': artist_id, 'target': x['uri'][15:]}})
     return nodes,edges
-    pass
-    # global df
-    # global udf
-    # r=(sp.artist_related_artists(artist_id)['artists'][0:depth])
-    # a = sp.artist(artist_id)
-    # udf.append(a['uri'])
-    # if maindict.get(a['name'])!=1:
-    #     for x in r:
-    #         df=df.append({'source':a['name'],'target':x['name']},ignore_index=True)
-    #         udf.append(x['uri'])
-    #     maindict[a['name']]=1
-    #     if width!=0:
-    #         for x in r:
-    #             addedge(x['uri'],depth,width-1)
-
-get_related('2h93pZq0e7k5yf4dywlkpM',20,0)
