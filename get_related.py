e1aab87f35e717430d7901a67e936310b936c85d
AllenThomasDev <allen.thomas.dev@gmail.com>
2021-04-06 22:17:34 +0530

    print statements removed from get_related.py

--- get_related.py
+++ get_related.py
@@ -23,11 +23,9 @@ def get_related(artist_id,depth,width=0):
         depth=20
     r=(sp.artist_related_artists(artist_id)['artists'])
     sorted_r=sorted(r, key = lambda i: i['followers']['total'],reverse=True)
-    print(sorted_r)
     a = sp.artist(artist_id)
     for x in sorted_r[0:depth]:
         nodes.append({'data': {'id': x['uri'][15:], 'label': x['name'],'url':x['images'][1]['url']}})
         edges.append({'data': {'source': artist_id, 'target': x['uri'][15:]}})
     return nodes,edges
 
-get_related('2h93pZq0e7k5yf4dywlkpM',10)
